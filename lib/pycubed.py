4b11343fd2747778775c456da64cde6f08ebc1fd
Will Christiansen <willchristiansen7@gmail.com>
2023-07-19 18:46:53 -0700

    test

--- lib/pycubed.py
+++ lib/pycubed.py
@@ -12,6 +12,10 @@ import busio, time, sys
 from storage import mount,umount,VfsFat
 from analogio import AnalogIn
 import digitalio, sdcardio, pwmio, tasko
+from digitalio import DigitalInOut, Direction
+from ssd1306 import SSD1306_I2C # save this library in PiSquare
+
+
 
 # Hardware Specific Libs
 import pycubed_rfm9x # Radio
@@ -130,6 +134,33 @@ class Satellite:
         #     self.hardware['Neopixel'] = True
         # except Exception as e:
         #     if self.debug: print('[WARNING][Neopixel]',e)
+        try:
+            self.led = DigitalInOut(board.GP24)
+            self.led.direction = Direction.OUTPUT
+        except Exception as e:
+            if self.debug:
+                print('[WARNING][LED]', e)
+
+        # OLED setup
+        try:
+            print(dir(board))
+            WIDTH = 128  # oled display width
+            HEIGHT = 32  # oled display height
+
+            i2c = busio.I2C(scl=board.GP7, sda=board.GP6, frequency=200000)
+
+            print(i2c)
+            print("I2C Address      : " + hex(i2c.scan()[0]).upper())  # Display device address
+            print("I2C Configuration: " + str(i2c))  # Display I2C config
+
+            oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)  # Init oled display
+
+            oled.fill(0)
+            oled.text("RS-0", 10, 5)
+            oled.show()
+        except Exception as e:
+            if self.debug:
+                print('[WARNING][OLED]', e)
 
         # Initialize USB charger
         # try:
